dff213aa2f6b7b6ce979a5d7eacc95285c9a8444
Myeong Woo Kim <mkim0628@gmail.com>
2024-07-03 14:54:01 +0900

    [refactor] 28p

--- Baseball_TDD/baseball.cpp
+++ Baseball_TDD/baseball.cpp
@@ -5,6 +5,8 @@ using namespace std;
 class Baseball {
 public:
 	void guess(const string& str) {
-		throw length_error("Must be three letters.");
+		if (str.length() != 3) {	
+			throw length_error("Must be three letters.");
+		}
 	}
 };
