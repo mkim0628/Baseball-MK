1e19894737627a9e71de756715d914076b845df2
Myeong Woo Kim <mkim0628@gmail.com>
2024-07-03 14:56:31 +0900

    [feature] 30p

--- Baseball_TDD/baseball.cpp
+++ Baseball_TDD/baseball.cpp
@@ -8,5 +8,10 @@ public:
 		if (str.length() != 3) {	
 			throw length_error("Must be three letters.");
 		}
+		for (char ch : str) {
+			if (ch < '0' || ch > '9') {
+				throw invalid_argument("Must be dddnumber");
+			}
+		}
 	}
 };
--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -6,3 +6,8 @@ TEST(BaseballTest, ThrowExceptionWhenInputLengthIsUnmatched) {
 	Baseball game;
 	EXPECT_THROW(game.guess(string("12")), length_error);
 }
+
+TEST(BaseballTest, ThrowExceptionWhenInvalidChar) {
+	Baseball game;
+	EXPECT_THROW(game.guess(string("12s")), invalid_argument);
+}
